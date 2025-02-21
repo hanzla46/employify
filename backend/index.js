393755a5cb86cf536f8d6a8bc21f97d8bb056932
hanzla-dev <hanzlaqamar808@gmail.com>
2025-02-21 23:59:57 +0500

    69.3

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,20 @@ const PORT = process.env.PORT || 8000;
 
 let dev_env = false;
 
-  app.use(
-    cors({
-      origin: dev_env ? "http://localhost:5173" : "https://employify.vercel.app",
-      methods: ["GET", "POST"],
-      credentials: true,
-      allowedHeaders: 'Content-Type,Authorization' 
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: dev_env ? "http://localhost:5173" : "https://employify.vercel.app",
+  //     methods: ["GET", "POST"],
+  //     credentials: true,
+  //     allowedHeaders: 'Content-Type,Authorization' 
+  //   })
+  // );
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
