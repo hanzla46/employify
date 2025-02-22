093fc8f1f5a426cbd4fcc8b0bd3787d9f5295e04
Hanzla <109482239+hanzla46@users.noreply.github.com>
2025-02-22 12:59:43 +0500

    Proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy:{
+      '/api':'https://employify-backend.vercel.app'
+    }
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
