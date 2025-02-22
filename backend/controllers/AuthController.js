e86ea08c129d6f1ff8e0f76cd0e51634a8e7c57d
hanzla-dev <hanzlaqamar808@gmail.com>
2025-02-22 13:49:17 +0500

    changee

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -13,7 +13,7 @@ const signup = async (req, res) => {
     const { name, email, password } = req.body;
     const user = await UserModel.findOne({ email });
     if (user) {
-      return res.status(409).json({ message: "User already exists!" });
+      return res.status(409).json({ message: "User already exists!!" });
     }
     const userModel = new UserModel({ name, email, password });
     userModel.password = await bcrypt.hash(password, 10);
