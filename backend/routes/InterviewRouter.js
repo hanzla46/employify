d2d24f47ec537d663ae4c9baae372b094ce744cd
hanzla-dev <hanzlaqamar808@gmail.com>
2025-03-02 12:40:45 +0500

    get

--- backend/routes/InterviewRouter.js
+++ backend/routes/InterviewRouter.js
@@ -1,8 +1,6 @@
 const {startInterview, continueInterview} = require("../controllers/InterviewController.js");
 const {ensureAuthenticated} = require("../middlewares/Auth");
 const router = require("express").Router();
-router.post("/start", ensureAuthenticated, (req,res) =>{
-    res.status(201).json({success: true, message: "starting interview..."});
-});
-router.post("/continue", ensureAuthenticated, continueInterview);
+router.get("/start", ensureAuthenticated, startInterview);
+router.get("/continue", ensureAuthenticated, continueInterview);
 module.exports = router;
