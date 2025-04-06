6206b7800e1706684abba3c0d3d68e01fc0dfb71
hanzla-dev <hanzlaqamar46@gmail.com>
2025-04-07 01:27:07 +0500

    rftgbhh

--- backend/routes/RoadmapRouter.js
+++ backend/routes/RoadmapRouter.js
@@ -2,5 +2,5 @@ const {generateRoadmap} = require("../controllers/RoadmapController");
 const upload = require("../middlewares/uploadMulter");
 const router = require("express").Router();
 const ensureAuthenticated = require("../middlewares/Auth");
-router.post("/get", ensureAuthenticated, upload.single("video"), generateRoadmap);
+router.post("/get", ensureAuthenticated, upload.single("file"), generateRoadmap);
 module.exports = router;
